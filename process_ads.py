088781b15fc9f7ec50cfa24e1524e07be85f5942
phuhh98 <phuhh98@gmail.com>
2023-12-12 02:07:04 +0700

    ads model adjust

--- process_ads.py
+++ process_ads.py
@@ -7,30 +7,13 @@ import statsmodels.formula.api as smf
 
 df = pd.read_csv("./data/ads_data.csv", index_col=0)
 
-INDEPENDENT_VARS = df["TV"]
-DEPENDENT_VARS = df["sales"]
-
-
-print(df.info())
+# print(df.info())
 
 # Take a look to see what sales could possibly depend on
 # look for high correlation
-print(df.corr())
+# print(df.corr())
 
 # Multi regression with OLS method
-results = smf.ols('sales ~ TV + radio + newspaper', data=df).fit()
+results = smf.ols(
+    'np.log(sales) ~ np.log(TV) + radio + newspaper', data=df).fit()
 print(results.summary())
-
-
-# def model(x, intercept, slope):
-#     return (np.e * intercept)*x**slope
-
-
-# plot.title("sales dependence on TV ads")
-# plot.xlabel("TV ads")
-# plot.ylabel("sales")
-# plot.yscale("log")
-
-# plot.scatter(INDEPENDENT_VARS, DEPENDENT_VARS)
-# plot.scatter(INDEPENDENT_VARS, model(INDEPENDENT_VARS, *results.params))
-# plot.show()
