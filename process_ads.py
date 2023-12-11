e181b442bbd0883d76270bc332b7653586737047
phuhh98 <phuhh98@gmail.com>
2023-12-11 23:57:53 +0700

    use statsmodels to run regression with ols method

--- process_ads.py
+++ process_ads.py
@@ -2,24 +2,27 @@
 
 import pandas as pd
 import matplotlib.pyplot as plot
-import numpy
-from scipy import stats
+import numpy as np
+import statsmodels.formula.api as smf
 
-df = pd.read_csv("./data/ads_data.csv")
-df.rename(index={0: "No."})
+df = pd.read_csv("./data/ads_data.csv", index_col=0)
 
 INDEPENDENT_VARS = df["TV"]
 DEPENDENT_VARS = df["sales"]
 
-slope, intercept, r, p, std_err = stats.linregress(
-    numpy.log(INDEPENDENT_VARS), numpy.log(DEPENDENT_VARS))
 
-print({"slope": slope, "intercept": intercept, "r": r, "p": p,
-      "std_err": std_err, "euler_number": intercept*numpy.e})
+print(df.info())
+
+# Take a look to see what sales could possibly depend on
+# look for high correlation
+print(df.corr())
+
+results = smf.ols('np.log(sales) ~ np.log(TV)', data=df).fit()
+print(results.summary())
 
 
-def model(x):
-    return (numpy.e * intercept)*x**slope
+def model(x, intercept, slope):
+    return (np.e * intercept)*x**slope
 
 
 plot.title("sales dependence on TV ads")
@@ -28,5 +31,5 @@ plot.ylabel("sales")
 plot.yscale("log")
 
 plot.scatter(INDEPENDENT_VARS, DEPENDENT_VARS)
-plot.scatter(INDEPENDENT_VARS, model(INDEPENDENT_VARS))
+plot.scatter(INDEPENDENT_VARS, model(INDEPENDENT_VARS, *results.params))
 plot.show()
